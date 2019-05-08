5f92950990f6c2d256fc461ec0dad1b0a0891bd4
wolframkriesing <wolfram+github@kriesing.de>
2019-05-08 09:39:05 +0200

    Make the textarea render filling out all space.

--- src/HcJsEditor.js
+++ src/HcJsEditor.js
@@ -4,6 +4,12 @@ template.innerHTML = `
     :host {
     	display: block;
     }
+    textarea {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
     </style>
     <textarea></textarea>
 `;
