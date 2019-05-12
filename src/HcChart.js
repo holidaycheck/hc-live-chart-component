a36edf995cd2b43e8b7047ab8778a79c51c5230c
wolframkriesing <wolfram+github@kriesing.de>
2019-05-12 12:32:17 +0200

    Fix the waterfall chart, it showed wrong bars.

--- src/HcChart.js
+++ src/HcChart.js
@@ -64,6 +64,10 @@ class HcChart extends HTMLElement {
             }
         });
     }
+    
+    /**
+    *	data: [{value 10, label: "something"}, ...]
+    */
     updateChartData(data) {
         const chartable = this.chartData;
         chartable.labels = data.map(({label}) => label.length > 30 ? ('...' + label.substring(label.length-30)) : label);
@@ -71,27 +75,39 @@ class HcChart extends HTMLElement {
         chartable.datasets[0].data = data.map(d => d.value);
         this.chart.update();
     }
+    
     /**
     *	data: [{start: 5, end 10, label: "something"}, ...]
     */
     updateWaterfallData(data) {
-		this.chart.options.scales.yAxes = [{stacked: true}];
-		this.chart.options.scales.xAxes = [{stacked: true}];
-		this.chart.options.tooltips = {
-            callbacks: {
-                label: ({datasetIndex, index}, {datasets}) => {
-                	if (datasetIndex === 0) return ''; // no tooltip item
-                	return `duration: ` + (datasets[1].data[index] - datasets[0].data[index]).toFixed(1); 
-                }
-            }
-		};
-
+        // Using stacked bar chart for showing this we have to do some calculations for
+        // showing the values properly. E.g. a value from 10 to 25 we must pass the data to the 
+        // chart as 10 in the first dataset and 15 in the second dataset. The charts render the 
+        // relative amount (15), not the absolute number (25).
+        // And in the tooltip we have to make sure to calculate back :).
+        this._waterfallOptions();
         const chartable = this.chartData;
         chartable.labels = data.map(s => s.label);
         chartable.datasets = [{data: [], backgroundColor: 'transparent'}, {data: [], backgroundColor: chartColors}];
         chartable.datasets[0].data = data.map(s => s.start);
-        chartable.datasets[1].data = data.map(s => s.end);
+        chartable.datasets[1].data = data.map(s => s.end - s.start); // subtract start, since we show stacked values
         this.chart.update();
+    }
+    _waterfallOptions() {
+        const tooltipLabel = ({datasetIndex, index}, {datasets}) => {
+            if (datasetIndex === 0) return ''; // no tooltip item
+            const start = datasets[0].data[index];
+            const end = datasets[1].data[index] + start; // add the start again, since we have stacked values
+            return `duration: ${(end - start).toFixed(0)} (${start.toFixed(0)}...${end.toFixed(0)})`;
+        };
+        
+        this.chart.options.scales.yAxes = [{stacked: true}];
+        this.chart.options.scales.xAxes = [{stacked: true}];
+        this.chart.options.tooltips = {
+            callbacks: {
+                label: tooltipLabel,
+            }
+		};
     }
 }
 
