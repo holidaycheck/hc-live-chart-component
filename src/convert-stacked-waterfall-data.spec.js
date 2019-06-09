5cf7cea371a0518d7d955f932267752dd1793974
wolframkriesing <wolfram+github@kriesing.de>
2019-06-09 02:04:58 +0200

    Implement more strange cases. Ready to refactor.

--- src/convert-stacked-waterfall-data.spec.js
+++ src/convert-stacked-waterfall-data.spec.js
@@ -12,7 +12,12 @@ const convert = (data, colors = noop) => {
             datasets.push({data: diffValues.map(v => v[1]), backgroundColor: colors(0)});
             datasets.push({data: diffValues.map(v => v[2]), backgroundColor: colors(1)});
             datasets.push({data: diffValues.map(v => v[3]), backgroundColor: colors(2)});
-        } else if (data.length) {
+        } else  if (data.length === 8) {
+            datasets.push({data: diffValues.map(v => v[1]), backgroundColor: colors(0)});
+            datasets.push({data: diffValues.map(v => v[2]), backgroundColor: colors(1)});
+            datasets.push({data: diffValues.map(v => v[3]), backgroundColor: colors(2)});
+            datasets.push({data: diffValues.map(v => v[4]), backgroundColor: colors(3)});
+        } else {
             if (data[0].values.length > 1 || (data[1] && data[1].values.length > 1) || (data[2] && data[2].values.length > 1)) {
                 datasets.push({data: diffValues.map(v => v[1]), backgroundColor: colors(0)});
             }
@@ -22,7 +27,7 @@ const convert = (data, colors = noop) => {
 };
 
 describe('Convert "stacked waterfall" values to chartable data', () => {
-    const colors = (index) => ['color-one', 'color-two', 'color-three',][index];
+    const colors = (index) => ['color-one', 'color-two', 'color-three', 'color-four',][index];
     
     it('empty data must return a valid chartable structure', () => {
         const noChartableData = {labels: [], datasets: []};
@@ -115,7 +120,7 @@ describe('Convert "stacked waterfall" values to chartable data', () => {
         });
     });
     describe('with >2 data points, returns multiple bars in different colors', () => {
-        it('for multiple data points, where some have no value or one', () => {
+        it('for multiple data points, where some have no value or one (starting with none to many)', () => {
             const data = [
                 {label: 'none', values: []},
                 {label: 'one', values: [2]},
@@ -132,6 +137,49 @@ describe('Convert "stacked waterfall" values to chartable data', () => {
                 {data: [undefined,undefined,undefined,  undefined,  2,4], backgroundColor: 'color-three'},
             ];
             const chartableData = {labels: ['none', 'one', 'two', 'three', 'four', 'four1'], datasets};
+            assertThat(convert(data, colors).labels, equalTo(chartableData.labels));      
+            assertThat(convert(data, colors).datasets, equalTo(chartableData.datasets));      
+        });
+        it('for multiple data points, where some have no value or one (ending with none starting with many)', () => {
+            const data = [
+                {label: 'four1', values: [1,2,4,8]},
+                {label: 'four', values: [1,2,4,6]},
+                {label: 'three', values: [1,2,3]},
+                {label: 'two', values: [1,2]},
+                {label: 'one', values: [2]},
+                {label: 'none', values: []},
+            ];
+
+            const datasets = [
+                {data: [1,1,1,          1,          2,          undefined], backgroundColor: 'transparent'},
+                {data: [1,1,1,          1,          undefined,  undefined], backgroundColor: 'color-one'},
+                {data: [2,2,1,          undefined,  undefined,  undefined], backgroundColor: 'color-two'},
+                {data: [4,2,undefined,  undefined,  undefined,  undefined], backgroundColor: 'color-three'},
+            ];
+            const chartableData = {labels: ['four1', 'four', 'three', 'two', 'one', 'none'], datasets};
+            assertThat(convert(data, colors).labels, equalTo(chartableData.labels));      
+            assertThat(convert(data, colors).datasets, equalTo(chartableData.datasets));      
+        });
+        it('for multiple data points, where some have no value or one (mixed)', () => {
+            const data = [
+                {label: 'four1', values: [1,2,4,8,20]},
+                {label: 'none', values: []},
+                {label: 'four', values: [1,2,4,6]},
+                {label: 'two', values: [2]},
+                {label: 'three', values: [1,2,3]},
+                {label: 'two', values: [1,2]},
+                {label: 'one', values: [2]},
+                {label: 'none', values: []},
+            ];
+
+            const datasets = [
+                {data: [1,undefined,1,2,        1,          1,          2,          undefined], backgroundColor: 'transparent'},
+                {data: [1,undefined,1,undefined,1,          1,          undefined,  undefined], backgroundColor: 'color-one'},
+                {data: [2,undefined,2,undefined,1,          undefined,  undefined,  undefined], backgroundColor: 'color-two'},
+                {data: [4,undefined,2,undefined,undefined,  undefined,  undefined,  undefined], backgroundColor: 'color-three'},
+                {data: [12,undefined,undefined,undefined,undefined,undefined,undefined,undefined], backgroundColor: 'color-four'},
+            ];
+            const chartableData = {labels: ['four1', 'none', 'four', 'two', 'three', 'two', 'one', 'none'], datasets};
             assertThat(convert(data, colors).labels, equalTo(chartableData.labels));      
             assertThat(convert(data, colors).datasets, equalTo(chartableData.datasets));      
         });
